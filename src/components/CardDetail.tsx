2d03d0c135240cb6c31e1d1cd2bc97debb7956b8
Federico Barbieri <federicobarbieri54@gmail.com>
2024-09-23 12:25:51 +0200

    refactor remove react

--- src/components/CardDetail.tsx
+++ src/components/CardDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import apiClient from "../services/apiClient";
 import { Card, CardBody, Heading, Image, Text, Flex, Box } from '@chakra-ui/react'
