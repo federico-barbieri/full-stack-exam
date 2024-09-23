0d77f28701495c4bd03bc963afa764b2ee0bd698
Federico Barbieri <federicobarbieri54@gmail.com>
2024-09-23 09:51:14 +0200

    add vertical filter nav

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <>
      <NavBar />
-     <h1>SMK Art</h1>
 
       <Grid />
       
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -2,6 +2,14 @@ import apiClient from "../services/apiClient";
 import {useState, useEffect} from "react";
 import './Grid.css';
 import ArtCard from "./ArtCard";
+import {
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    AccordionIcon,
+    Box,
+  } from '@chakra-ui/react'
 
 
 
@@ -26,25 +34,73 @@ const Grid = () => {
     }, []);
 
     return (
-        <div className="cardGrid">
-            
-            <ul>
 
-                {art.map((artItem) => (
-                    <li key={artItem.id}>
-                        <ArtCard 
-                            titles={artItem.titles} 
-                            artist={artItem.artist} 
-                            has_image={artItem.has_image} 
-                            image_thumbnail={artItem.image_thumbnail}
-                            materials={artItem.materials}
-                            techniques={artItem.techniques}
-                            colors={artItem.colors}
-                        />
-                    </li>
-                ))}
-            </ul>
-            {error && <p style={{ color: "red" }}>{error}</p>}
+        <div className="navAndGrid">
+            <Accordion width="30%">
+                <AccordionItem>
+                    <h2>
+                    <AccordionButton>
+                        <Box as='span' flex='1' textAlign='left'>
+                        Filter by genre
+                        </Box>
+                        <AccordionIcon />
+                    </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4} >
+                    <Box as='h3' flex='1' textAlign='left'>
+                        Modern
+                    </Box>
+                    <Box as='h3' flex='1' textAlign='left'>
+                        Danish
+                    </Box>
+                    <Box as='h3' flex='1' textAlign='left'>
+                        Udenlands
+                    </Box>
+                    </AccordionPanel>
+                </AccordionItem>
+
+                <AccordionItem>
+                    <h2>
+                    <AccordionButton>
+                        <Box as='span' flex='1' textAlign='left'>
+                        Filter by artist
+                        </Box>
+                        <AccordionIcon />
+                    </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4} >
+                    <Box as='h3' flex='1' textAlign='left'>
+                        Chagal
+                    </Box>
+                    <Box as='h3' flex='1' textAlign='left'>
+                        Modigliani
+                    </Box>
+                    <Box as='h3' flex='1' textAlign='left'>
+                        Munch
+                    </Box>
+                    </AccordionPanel>
+                </AccordionItem>
+
+            </Accordion>
+
+            <div className="cardGrid">
+                <ul>
+                    {art.map((artItem) => (
+                        <li key={artItem.id}>
+                            <ArtCard 
+                                titles={artItem.titles} 
+                                artist={artItem.artist} 
+                                has_image={artItem.has_image} 
+                                image_thumbnail={artItem.image_thumbnail}
+                                materials={artItem.materials}
+                                techniques={artItem.techniques}
+                                colors={artItem.colors}
+                            />
+                        </li>
+                    ))}
+                </ul>
+                {error && <p style={{ color: "red" }}>{error}</p>}
+            </div>
         </div>
     )
 };
