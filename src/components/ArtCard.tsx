0acb3404885bfefc3a9c802dd73ff709f0f982f4
Federico Barbieri <federicobarbieri54@gmail.com>
2024-10-06 01:04:12 +0200

    responsive card 750px

--- src/components/ArtCard.tsx
+++ src/components/ArtCard.tsx
@@ -19,38 +19,38 @@ const ArtCard = ({ titles, artist, image_thumbnail, materials, techniques, color
 
     return (
         
-        <Card _hover={{filter: 'drop-shadow(2px 2px 2px black)'}} m="1rem" w="70%">
+        <Card className="individualCard" _hover={{filter: 'drop-shadow(2px 2px 2px black)'}} m="0rem" w="100%">
             <CardBody>
                 <Image 
                 src={image_thumbnail} 
                 alt={titles && titles.length > 0 ? titles[0].title : "no image available"} 
                 />
                 
-                <Box p={2} as='div' flex='1' display="flex" justifyContent="space-between" textAlign='left'>
+                <Box p={1} as='div' flex='1' display="flex" justifyContent="space-between" textAlign='left'>
                     <Box>
                         <Text p={0} textAlign="left" fontSize="lg"><strong>{titles && titles.length > 0 ? titles[0].title : "Untitled"}</strong></Text>
-                        <Text pt={2} textAlign="left" fontSize="md">By {artist || "Unknown Artist"}</Text>
-                        <Text pt={2} textAlign="left" fontSize="md">{production_date}</Text>
+                        <Text pt={1} textAlign="left" fontSize="md">By {artist || "Unknown Artist"}</Text>
+                        <Text pt={1} textAlign="left" fontSize="md">{production_date}</Text>
                     </Box>
 
-                    {getColors && <Text w="250px" p={2}>{getColors}</Text>}
+                    {getColors && <Text w="250px" p={1}>{getColors}</Text>}
 
                 </Box>
 
                 <Box display="flex" justifyContent="flex-end">
                     <Text 
                     style={{backgroundColor: '#6F9AAA', padding: '0.25rem', color: 'white', borderRadius: '20px'}} 
-                    w="30%" 
-                    p={2} 
-                    m={2} 
+                    w="50%" 
+                    p={0} 
+                    m={0} 
                     fontSize="sm">
                     {techniques}
                     </Text>
                     {materials && <Text 
                     style={{backgroundColor: '#6F9AAA', padding: '0.25rem', color: 'white', borderRadius: '20px'}} 
-                    w="30%" 
-                    p={2} 
-                    m={2} 
+                    w="50%" 
+                    p={0} 
+                    m={0} 
                     fontSize="sm">
                     {materials}
                     </Text> }
